908ec8228a5c5353048bfc3339390f41cbfd75dc
sakshijn16 <71726866+sakshi168@users.noreply.github.com>
2022-01-14 21:57:39 +0530

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,17 @@
+#Bubble sort in python
+
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        swapped = False
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            return
+ 
+ 
+arr = [int(x) for x in input("Enter numbers: ").split()]
+bubble_sort(arr)
+print('Sorted list: ', end='')
+print(arr)
